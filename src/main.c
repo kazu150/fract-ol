89286678719325a85e3cf2e5c6962704050d8532
Kazushi Isogai <kaisogai@c6r1s2.42tokyo.jp>
2025-07-22 16:53:26 +0900

    a

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: kaisogai <kaisogai@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 17:57:24 by kaisogai          #+#    #+#             */
-/*   Updated: 2025/07/21 21:10:03 by kaisogai         ###   ########.fr       */
+/*   Updated: 2025/07/22 16:51:40 by kaisogai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,28 +48,33 @@ void	draw(void *mlx, void *mlx_win)
 	int			i;
 	s_complex	z;
 	s_complex	res;
-	int			res_dist;
+	int			is_inside;
 
-	z = (s_complex){0, 0};
-	res = (s_complex){0, 0};
-	i = 0;
-	while (z.real < 100)
+	z = (s_complex){-2, -2};
+	while (z.real < 2)
 	{
-		z.real += 0.1;
-		z.imag = 0;
-		while (z.imag < 100)
+		z.real += 0.01;
+		z.imag = -1;
+		while (z.imag < 2)
 		{
-			z.imag += 0.1;
-			while (i < 10)
+			z.imag += 0.01;
+			i = 0;
+			res = (s_complex){0, 0};
+			is_inside = 1;
+			while (i < 20)
 			{
 				res = complex_mul(res, res);
 				res = complex_add(res, z);
+				if (sqrt(res.real * res.real + res.imag * res.imag) > 2)
+				{
+					is_inside = 0;
+					break ;
+				}
 				i++;
 			}
-			printf("[%f,%f]", res.real, res.imag);
-			res_dist = sqrt(res.real * res.real + res.imag * res.imag);
-			if (res_dist > 2)
-				mlx_pixel_put(mlx, mlx_win, z.real * 10, z.imag * 10, 0xFFFFFF);
+			if (is_inside)
+				mlx_pixel_put(mlx, mlx_win, z.real * 100 + 250, z.imag * 100
+					+ 250, 0xFFFFFF);
 		}
 	}
 }
@@ -80,8 +85,8 @@ int	main(void)
 	void	*mlx_win;
 
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 300, 300, "Hello World");
-	mlx_hook(mlx_win, KeyPress, KeyPressMask, event_handler, mlx);
+	mlx_win = mlx_new_window(mlx, 500, 500, "fractol");
+	mlx_hook(mlx_win, FocusIn, FocusChangeMask, event_handler, mlx);
 	draw(mlx, mlx_win);
 	mlx_loop(mlx);
 }
