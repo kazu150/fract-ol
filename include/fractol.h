ecd10c919a220dfcaa944aa88d262127b0eb40da
Kazushi Isogai <kaisogai@c5r4s5.42tokyo.jp>
2025-07-26 17:46:57 +0900

    a

--- include/fractol.h
+++ include/fractol.h
@@ -6,20 +6,41 @@
 /*   By: kaisogai <kaisogai@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 17:58:10 by kaisogai          #+#    #+#             */
-/*   Updated: 2025/07/22 17:45:12 by kaisogai         ###   ########.fr       */
+/*   Updated: 2025/07/26 15:45:37 by kaisogai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
 # define FRACTOL_H
 
+# include "../include/fractol.h"
 # include "../libft/libft.h"
-#include "../include/fractol.h"
-#include "../mlx/mlx.h"
-#include <X11/X.h>
-#include <X11/keysym.h>
-#include <math.h>
-#include <stdio.h>
+# include "../mlx/mlx.h"
+# include <X11/X.h>
+# include <X11/keysym.h>
+# include <math.h>
+# include <stdio.h>
+
+# define WIDTH 600
+# define HEIGHT 600
+
+typedef struct s_img
+{
+	void *img_ptr; // mlx_new_image() の戻り値を格納
+	char *data;    // 画像バッファ
+	int bpp;       // bits per pixel
+	int size_line; // 1行のバイト数
+	int endian;    // エンディアン
+}			t_img;
+typedef struct s_vars
+{
+	void	*mlx;
+	void	*win;
+	double	zoom;
+	double	x_offset;
+	double	y_offset;
+	t_img	img;
+}			t_vars;
 
 typedef struct t_complex
 {
--- src/main.c
+++ src/main.c
@@ -6,22 +6,11 @@
 /*   By: kaisogai <kaisogai@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 17:57:24 by kaisogai          #+#    #+#             */
-/*   Updated: 2025/07/25 19:18:21 by kaisogai         ###   ########.fr       */
+/*   Updated: 2025/07/26 17:40:17 by kaisogai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/fractol.h"
-
-typedef struct s_vars
-{
-	void	*mlx;
-	void	*win;
-	int		width;
-	int		height;
-	double	zoom;
-	int		x_center;
-	int		y_center;
-}			t_vars;
 
 s_complex	complex_mul(s_complex a, s_complex b)
 {
@@ -34,25 +23,6 @@ s_complex	complex_add(s_complex a, s_complex b)
 	return ((s_complex){a.real + b.real, a.imag + b.imag});
 }
 
-int	calc_center(double zoom, int size, int position)
-{
-	int	center;
-
-	if (zoom > 1)
-		center = size / 2 - (position - size / 2) / 2 * zoom;
-	else
-		center = size / 2;
-	return (center);
-}
-
-double	offset(double zoom, int size, int position)
-{
-	double	o;
-
-	o = (double)calc_center(zoom, size, position) / (double)size;
-	return (o);
-}
-
 int	color(int i)
 {
 	if (i < 5)
@@ -61,33 +31,51 @@ int	color(int i)
 		return (0x666666);
 	return (0xbbbbbb);
 }
+
+void	put_to_image(t_img *img, int x, int y, int color)
+{
+	char	*dst;
+
+	if (x < 0 || y < 0 || x >= WIDTH || y >= HEIGHT)
+		return ;
+	dst = img->data + (y * img->size_line + x * (img->bpp / 8));
+	*(unsigned int *)dst = color;
+}
+
 void	draw(t_vars *v)
 {
 	int			i;
 	s_complex	z;
 	s_complex	res;
 	int			is_inside;
+	double		real_max;
+	double		imag_max;
+	int			x;
+	int			y;
 
-	z.real = (double)(v->x_center - v->width / 2) / v->width / v->zoom - 2
-		/ v->zoom;
-	z.imag = (double)(v->y_center - v->height / 2) / v->height / v->zoom - 2
-		/ v->zoom;
-	printf("centr[%d,%d]\n", v->x_center, v->y_center);
-	printf("zahyo[%f,%f]\n", z.real, z.imag);
+	z.real = ((WIDTH / 2 - v->x_offset) / WIDTH - 2) / v->zoom;
+	real_max = z.real + 4 / v->zoom;
+	z.imag = ((HEIGHT / 2 - v->y_offset) / HEIGHT - 2) / v->zoom;
+	imag_max = z.imag + 4 / v->zoom;
+	printf("zoom:%f\n", v->zoom);
+	printf("offset[%f,%f]\n", v->x_offset, v->y_offset);
+	printf("real[%f,%f,%f]\n", z.real, real_max, real_max - z.real);
+	printf("imag[%f,%f,%f]\n", z.imag, imag_max, imag_max - z.imag);
 	fflush(stdout);
-	while (z.real < 2 / v->zoom - (double)(v->x_center - v->width / 2) / 100
-		* v->zoom)
+	v->img.img_ptr = mlx_new_image(v->mlx, WIDTH, HEIGHT);
+	v->img.data = mlx_get_data_addr(v->img.img_ptr, &v->img.bpp,
+			&v->img.size_line, &v->img.endian);
+	while (z.real < real_max)
 	{
 		z.real += 0.01 / v->zoom;
 		z.imag = -2;
-		while (z.imag < 2 / v->zoom - (double)(v->y_center - v->height / 2)
-			/ 100 * v->zoom)
+		while (z.imag < imag_max)
 		{
 			z.imag += 0.01 / v->zoom;
 			i = 0;
 			res = (s_complex){0, 0};
 			is_inside = 1;
-			while (i < 30)
+			while (i < 100)
 			{
 				res = complex_mul(res, res);
 				res = complex_add(res, z);
@@ -99,24 +87,27 @@ void	draw(t_vars *v)
 				i++;
 			}
 			if (!is_inside)
-				mlx_pixel_put(v->mlx, v->win, z.real * v->zoom * 100
-					+ v->x_center, z.imag * v->zoom * 100 + v->y_center,
-					color(i));
+			{
+				x = z.real * v->zoom * 100 + v->x_offset;
+				y = z.imag * v->zoom * 100 + v->y_offset;
+				put_to_image(&v->img, x, y, color(i));
+			}
 		}
 	}
+	mlx_put_image_to_window(v->mlx, v->win, v->img.img_ptr, 0, 0);
 }
 
-void	update_coordinate_center(int x, int y, t_vars *v)
+void	update_coordinate_offset(int x, int y, t_vars *v)
 {
 	if (v->zoom > 1)
 	{
-		v->x_center = v->x_center - (x - v->width / 2);
-		v->y_center = v->y_center - (y - v->height / 2);
+		v->x_offset = v->x_offset - (x - WIDTH / 2) / v->zoom;
+		v->y_offset = v->y_offset - (y - HEIGHT / 2) / v->zoom;
 	}
 	else
 	{
-		v->x_center = v->width / 2;
-		v->y_center = v->height / 2;
+		v->x_offset = WIDTH / 2;
+		v->y_offset = HEIGHT / 2;
 	}
 }
 
@@ -130,7 +121,7 @@ int	event_handler(int button, int x, int y, void *param)
 		mlx_clear_window(v->mlx, v->win);
 		if (v->zoom > 1)
 			v->zoom /= 1.2;
-		update_coordinate_center(x, y, v);
+		update_coordinate_offset(x, y, v);
 		draw(v);
 	}
 	else if (button == 5)
@@ -138,7 +129,7 @@ int	event_handler(int button, int x, int y, void *param)
 		mlx_clear_window(v->mlx, v->win);
 		if (v->zoom < 100)
 			v->zoom *= 1.2;
-		update_coordinate_center(x, y, v);
+		update_coordinate_offset(x, y, v);
 		draw(v);
 	}
 	return (0);
@@ -149,13 +140,11 @@ int	main(void)
 	t_vars	*v;
 
 	v = malloc(sizeof(t_vars));
-	v->width = 600;
-	v->height = 600;
 	v->zoom = 1;
 	v->mlx = mlx_init();
-	v->win = mlx_new_window(v->mlx, v->width, v->height, "fractol");
-	v->x_center = v->width / 2;
-	v->y_center = v->height / 2;
+	v->win = mlx_new_window(v->mlx, WIDTH, HEIGHT, "fractol");
+	v->x_offset = WIDTH / 2;
+	v->y_offset = HEIGHT / 2;
 	mlx_mouse_hook(v->win, event_handler, v);
 	draw(v);
 	mlx_loop(v->mlx);
