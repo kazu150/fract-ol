d1c5179a815564bec55bb821b9147b878a91316a
Kazushi Isogai <kaisogai@c1r6s8.42tokyo.jp>
2025-07-28 19:02:36 +0900

    a

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: kaisogai <kaisogai@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 17:57:24 by kaisogai          #+#    #+#             */
-/*   Updated: 2025/07/26 17:40:17 by kaisogai         ###   ########.fr       */
+/*   Updated: 2025/07/28 19:01:59 by kaisogai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,27 @@ s_complex	complex_add(s_complex a, s_complex b)
 
 int	color(int i)
 {
+	if (i < 3)
+		return (0xffc1e0);
 	if (i < 5)
-		return (0x333333);
+		return (0xffc1ff);
+	if (i < 7)
+		return (0xe0c1ff);
 	if (i < 10)
-		return (0x666666);
-	return (0xbbbbbb);
+		return (0xc1c1ff);
+	if (i < 13)
+		return (0xc1e0ff);
+	if (i < 16)
+		return (0xc1ffff);
+	if (i < 20)
+		return (0xc1ffe0);
+	if (i < 27)
+		return (0xc1ffc1);
+	if (i < 40)
+		return (0xe0ffc1);
+	if (i < 50)
+		return (0xffffc1);
+	return (0xffe0c1);
 }
 
 void	put_to_image(t_img *img, int x, int y, int color)
@@ -46,40 +62,39 @@ void	draw(t_vars *v)
 {
 	int			i;
 	s_complex	z;
-	s_complex	res;
+	s_complex	c;
 	int			is_inside;
-	double		real_max;
-	double		imag_max;
+	s_complex	min;
+	s_complex	max;
 	int			x;
 	int			y;
+	double		scale;
 
-	z.real = ((WIDTH / 2 - v->x_offset) / WIDTH - 2) / v->zoom;
-	real_max = z.real + 4 / v->zoom;
-	z.imag = ((HEIGHT / 2 - v->y_offset) / HEIGHT - 2) / v->zoom;
-	imag_max = z.imag + 4 / v->zoom;
-	printf("zoom:%f\n", v->zoom);
-	printf("offset[%f,%f]\n", v->x_offset, v->y_offset);
-	printf("real[%f,%f,%f]\n", z.real, real_max, real_max - z.real);
-	printf("imag[%f,%f,%f]\n", z.imag, imag_max, imag_max - z.imag);
-	fflush(stdout);
+	x = 0;
+	y = 0;
+	min.real = ((WIDTH / 2 - v->x_offset) / WIDTH - 2) / v->zoom;
+	min.imag = ((HEIGHT / 2 - v->y_offset) / HEIGHT - 2) / v->zoom;
+	max.real = min.real + 4 / v->zoom;
+	max.imag = min.imag + 4 / v->zoom;
 	v->img.img_ptr = mlx_new_image(v->mlx, WIDTH, HEIGHT);
 	v->img.data = mlx_get_data_addr(v->img.img_ptr, &v->img.bpp,
 			&v->img.size_line, &v->img.endian);
-	while (z.real < real_max)
+	z.real = min.real;
+	scale = 4.0 / (WIDTH * v->zoom);
+	while (x < WIDTH)
 	{
-		z.real += 0.01 / v->zoom;
-		z.imag = -2;
-		while (z.imag < imag_max)
+		y = 0;
+		while (y < HEIGHT)
 		{
-			z.imag += 0.01 / v->zoom;
+			z.real = (x - WIDTH / 2.0 + v->x_offset) * scale;
+			z.imag = (y - HEIGHT / 2.0 + v->y_offset) * scale;
 			i = 0;
-			res = (s_complex){0, 0};
+			c = (s_complex){0, 0};
 			is_inside = 1;
 			while (i < 100)
 			{
-				res = complex_mul(res, res);
-				res = complex_add(res, z);
-				if (sqrt(res.real * res.real + res.imag * res.imag) > 2)
+				c = complex_add(complex_mul(c, c), z);
+				if (sqrt(c.real * c.real + c.imag * c.imag) > 2)
 				{
 					is_inside = 0;
 					break ;
@@ -88,26 +103,37 @@ void	draw(t_vars *v)
 			}
 			if (!is_inside)
 			{
-				x = z.real * v->zoom * 100 + v->x_offset;
-				y = z.imag * v->zoom * 100 + v->y_offset;
 				put_to_image(&v->img, x, y, color(i));
 			}
+			y++;
 		}
+		x++;
 	}
+	printf("zoom:%f\n", v->zoom);
+	printf("offset[%f,%f]\n", v->x_offset, v->y_offset);
+	fflush(stdout);
 	mlx_put_image_to_window(v->mlx, v->win, v->img.img_ptr, 0, 0);
 }
 
-void	update_coordinate_offset(int x, int y, t_vars *v)
+void	update_coordinate_offset(int x, int y, t_vars *v, int is_zoom_out)
 {
-	if (v->zoom > 1)
+	if (is_zoom_out)
 	{
-		v->x_offset = v->x_offset - (x - WIDTH / 2) / v->zoom;
-		v->y_offset = v->y_offset - (y - HEIGHT / 2) / v->zoom;
+		if (v->zoom > 1)
+		{
+			v->x_offset = v->x_offset / 1.2;
+			v->y_offset = v->y_offset / 1.2;
+		}
+		else
+		{
+			v->x_offset = 0;
+			v->y_offset = 0;
+		}
 	}
 	else
 	{
-		v->x_offset = WIDTH / 2;
-		v->y_offset = HEIGHT / 2;
+		v->x_offset = v->x_offset + (x - WIDTH / 2) * v->zoom / 2;
+		v->y_offset = v->y_offset + (y - HEIGHT / 2) * v->zoom / 2;
 	}
 }
 
@@ -118,18 +144,16 @@ int	event_handler(int button, int x, int y, void *param)
 	v = (t_vars *)param;
 	if (button == 4)
 	{
-		mlx_clear_window(v->mlx, v->win);
 		if (v->zoom > 1)
 			v->zoom /= 1.2;
-		update_coordinate_offset(x, y, v);
+		update_coordinate_offset(x, y, v, 1);
 		draw(v);
 	}
 	else if (button == 5)
 	{
-		mlx_clear_window(v->mlx, v->win);
 		if (v->zoom < 100)
 			v->zoom *= 1.2;
-		update_coordinate_offset(x, y, v);
+		update_coordinate_offset(x, y, v, 0);
 		draw(v);
 	}
 	return (0);
@@ -143,8 +167,8 @@ int	main(void)
 	v->zoom = 1;
 	v->mlx = mlx_init();
 	v->win = mlx_new_window(v->mlx, WIDTH, HEIGHT, "fractol");
-	v->x_offset = WIDTH / 2;
-	v->y_offset = HEIGHT / 2;
+	v->x_offset = 0;
+	v->y_offset = 0;
 	mlx_mouse_hook(v->win, event_handler, v);
 	draw(v);
 	mlx_loop(v->mlx);
