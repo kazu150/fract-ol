4c15d6e7741167cc3afb58d4444d80b6d09f8e49
Kazushi Isogai <kaisogai@c6r3s6.42tokyo.jp>
2025-08-01 18:58:38 +0900

    a

---
+++ src/handlers.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handlers.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaisogai <kaisogai@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/01 18:53:17 by kaisogai          #+#    #+#             */
+/*   Updated: 2025/08/01 18:58:03 by kaisogai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/fractol.h"
+
+static void	update_coordinate_offset(int x, int y, t_vars *v, int is_zoom_out)
+{
+	if (is_zoom_out)
+	{
+		if (v->zoom > 1)
+		{
+			v->x_offset = v->x_offset / ZOOM_LEVEL + (x - WIDTH / 2);
+			v->y_offset = v->y_offset / ZOOM_LEVEL + (y - HEIGHT / 2);
+		}
+		else
+		{
+			v->x_offset = 0;
+			v->y_offset = 0;
+		}
+	}
+	else
+	{
+		v->x_offset = v->x_offset * ZOOM_LEVEL + (x - WIDTH / 2);
+		v->y_offset = v->y_offset * ZOOM_LEVEL + (y - HEIGHT / 2);
+	}
+}
+
+static void	clean_exit(t_vars *v)
+{
+	if (v->img.img_ptr)
+		mlx_destroy_image(v->mlx, v->img.img_ptr);
+	if (v->win)
+		mlx_destroy_window(v->mlx, v->win);
+	if (v->mlx)
+	{
+		mlx_destroy_display(v->mlx);
+		free(v->mlx);
+	}
+	exit(0);
+}
+
+int	handle_key(int keycode, t_vars *v)
+{
+	if (keycode == KEY_ESC)
+		clean_exit(v);
+	else
+	{
+		if (keycode == KEY_UP_ARROW)
+			v->y_offset = v->y_offset - KEY_MOVE_RANGE;
+		if (keycode == KEY_DOWN_ARROW)
+			v->y_offset = v->y_offset + KEY_MOVE_RANGE;
+		if (keycode == KEY_LEFT_ARROW)
+			v->x_offset = v->x_offset - KEY_MOVE_RANGE;
+		if (keycode == KEY_RIGHT_ARROW)
+			v->x_offset = v->x_offset + KEY_MOVE_RANGE;
+		if (keycode == KEY_1)
+			v->color_range_pattern = COLOR_RANGE_L;
+		if (keycode == KEY_2)
+			v->color_range_pattern = COLOR_RANGE_M;
+		if (keycode == KEY_3)
+			v->color_range_pattern = COLOR_RANGE_S;
+		draw(v);
+	}
+	return (0);
+}
+
+int	handle_close(t_vars *v)
+{
+	clean_exit(v);
+	return (0);
+}
+
+int	event_handler(int button, int x, int y, void *param)
+{
+	t_vars	*v;
+
+	v = (t_vars *)param;
+	if (button == 4)
+	{
+		if (v->zoom > 1)
+			v->zoom /= ZOOM_LEVEL;
+		update_coordinate_offset(x, y, v, 1);
+		draw(v);
+	}
+	else if (button == 5)
+	{
+		v->zoom *= ZOOM_LEVEL;
+		update_coordinate_offset(x, y, v, 0);
+		draw(v);
+	}
+	return (0);
+}
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: kaisogai <kaisogai@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 17:57:24 by kaisogai          #+#    #+#             */
-/*   Updated: 2025/08/01 18:40:37 by kaisogai         ###   ########.fr       */
+/*   Updated: 2025/08/01 18:57:52 by kaisogai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,101 +89,15 @@ void	draw(t_vars *v)
 	mlx_put_image_to_window(v->mlx, v->win, v->img.img_ptr, 0, 0);
 }
 
-void	update_coordinate_offset(int x, int y, t_vars *v, int is_zoom_out)
-{
-	if (is_zoom_out)
-	{
-		if (v->zoom > 1)
-		{
-			v->x_offset = v->x_offset / ZOOM_LEVEL + (x - WIDTH / 2);
-			v->y_offset = v->y_offset / ZOOM_LEVEL + (y - HEIGHT / 2);
-		}
-		else
-		{
-			v->x_offset = 0;
-			v->y_offset = 0;
-		}
-	}
-	else
-	{
-		v->x_offset = v->x_offset * ZOOM_LEVEL + (x - WIDTH / 2);
-		v->y_offset = v->y_offset * ZOOM_LEVEL + (y - HEIGHT / 2);
-	}
-}
-
-int	event_handler(int button, int x, int y, void *param)
-{
-	t_vars	*v;
-
-	v = (t_vars *)param;
-	if (button == 4)
-	{
-		if (v->zoom > 1)
-			v->zoom /= ZOOM_LEVEL;
-		update_coordinate_offset(x, y, v, 1);
-		draw(v);
-	}
-	else if (button == 5)
-	{
-		v->zoom *= ZOOM_LEVEL;
-		update_coordinate_offset(x, y, v, 0);
-		draw(v);
-	}
-	return (0);
-}
-
-static void	clean_exit(t_vars *v)
-{
-	if (v->img.img_ptr)
-		mlx_destroy_image(v->mlx, v->img.img_ptr);
-	if (v->win)
-		mlx_destroy_window(v->mlx, v->win);
-	if (v->mlx)
-	{
-		mlx_destroy_display(v->mlx);
-		free(v->mlx);
-	}
-	exit(0);
-}
-
-static int	handle_key(int keycode, t_vars *v)
-{
-	if (keycode == KEY_ESC)
-		clean_exit(v);
-	else
-	{
-		if (keycode == KEY_UP_ARROW)
-			v->y_offset = v->y_offset - 30;
-		if (keycode == KEY_DOWN_ARROW)
-			v->y_offset = v->y_offset + 30;
-		if (keycode == KEY_LEFT_ARROW)
-			v->x_offset = v->x_offset - 30;
-		if (keycode == KEY_RIGHT_ARROW)
-			v->x_offset = v->x_offset + 30;
-		if (keycode == KEY_1)
-			v->color_range_pattern = COLOR_RANGE_L;
-		if (keycode == KEY_2)
-			v->color_range_pattern = COLOR_RANGE_M;
-		if (keycode == KEY_3)
-			v->color_range_pattern = COLOR_RANGE_S;
-		draw(v);
-	}
-	return (0);
-}
-
-static int	handle_close(t_vars *v)
-{
-	clean_exit(v);
-	return (0);
-}
-
 int	main(int argc, char **argv)
 {
 	t_vars	*v;
 
 	if (invalid_param(argc, argv))
 	{
-		ft_printf("valid param list\nm: mandelbrot\nj real_num imaginary_num: julia\n");
+		ft_printf("valid param list\n");
+		ft_printf("m: mandelbrot set\n");
+		ft_printf("j real_num imaginary_num: julia set\n");
 		exit(0);
 	}
 	v = malloc(sizeof(t_vars));
